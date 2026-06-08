1cf130ea2cc626c2754d688eaec38066c43605d8
smriti-kumar <mailsmritikumar@gmail.com>
2026-06-08 16:53:18 -0400

    linearb + optimizer works

---
+++ linearb/linearb.py
@@ -0,0 +1,57 @@
+import torch
+import torch.nn as nn
+
+class LinearB(nn.Module):
+    def __init__(self, in_params, out_params, num_bases):
+        super().__init__()
+        current_device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.in_params = in_params
+        self.out_params = out_params
+        self.num_bases = num_bases
+        self.n = max(2, int(2 ** torch.ceil(torch.log2(torch.tensor(max(in_params, out_params))))))
+        binary_bits = torch.sign(torch.randn(num_bases, self.n * self.n, device=current_device))
+        self.binary_coeffs = torch.nn.Parameter(binary_bits, requires_grad=False)
+        self.binary_coeffs.is_quantized_basis = True
+        self.binary_coeffs.binary_coeffs = self.binary_coeffs
+        self.binary_coeffs.true_shape = (out_params, in_params)
+        self.bias = torch.nn.Parameter(torch.zeros(out_params, device=current_device))
+        self.bias.is_quantized_basis = False
+
+        U_Ls = []
+        U_Rs = []
+        for i in range(num_bases):
+            if i == 0:
+                U_Ls.append(torch.eye(self.n, device=current_device))
+                U_Rs.append(torch.eye(self.n, device=current_device))
+            else:
+                l = torch.randn(self.n, self.n, device=current_device)
+                U_l, _ = torch.linalg.qr(l)
+                r = torch.randn(self.n, self.n, device=current_device)
+                U_r, _ = torch.linalg.qr(r)
+                U_Ls.append(U_l)
+                U_Rs.append(U_r)
+        self.register_buffer("U_L", torch.stack(U_Ls))
+        self.register_buffer("U_R", torch.stack(U_Rs))
+        self.binary_coeffs.U_L = self.U_L
+        self.binary_coeffs.U_R = self.U_R
+    
+    def forward(self, x):
+        W = self.hb_transform(self.binary_coeffs)
+        W = self.U_L @ W @ self.U_R
+        W = torch.sum(W, dim=0)[:self.out_params, :self.in_params]
+        return x @ W.T + self.bias
+
+    def hb_transform(self, x):
+        if len(x.shape) == 1:
+            x = x.view(1,-1)
+        (m,n) = x.shape
+        k = 1
+        while 4**k < n:
+            k += 1
+        assert(4**k == n)
+        x = x.reshape((m,) + (4,)*k)
+        for i in range(k):
+            x = x.sum(1+i,keepdim=True) - 2*x
+        x = x.reshape((m,) + (2,2)*k)
+        x = x.permute((0,) + tuple(2*i+1 for i in range(k)) + tuple(2*i+2 for i in range(k)))
+        return x.reshape(m, 2**k, 2**k) / (2**k)
---
+++ linearb/optimizer.py
@@ -0,0 +1,62 @@
+import torch
+
+class OrthogonalOptimizer(torch.optim.Optimizer):
+    def __init__(self, params, lr=0.01, momentum=0.9, num_flips=1):
+        defaults = dict(lr=lr, momentum=momentum, num_flips=num_flips)
+        super().__init__(params, defaults)
+        for group in self.param_groups:
+            for p in group['params']:
+                self.state[p] = {}
+                
+    def step(self, closure=None):
+        loss = None
+        if closure is not None:
+            with torch.enable_grad():
+                loss = closure()
+        for group in self.param_groups:
+            for p in group['params']:
+                if p.grad is None:
+                    continue
+                if not getattr(p, 'is_quantized_basis', False): # gradient descent on biases and non linearb layers
+                    p.data -= group['lr'] * p.grad.data
+                    continue
+                num_bases, nxn = p.binary_coeffs.shape
+                n = int(nxn**0.5)
+                if "momentum_buffer" not in self.state[p]:
+                    self.state[p]["momentum_buffer"] = torch.zeros((n, n), dtype=torch.float32, device=p.device)
+                m = self.state[p]["momentum_buffer"]
+                row, col = p.true_shape
+                grad_padded = torch.zeros((n, n), dtype=torch.float32, device=p.device)
+                grad_padded[:row, :col] = p.grad.data
+                m = group["momentum"] * m + (1 - group["momentum"]) * grad_padded
+                m[row:, :] = 0
+                m[:, col:] = 0
+                self.state[p]["momentum_buffer"] = m
+                for i in range(num_bases):
+                    c = p.binary_coeffs[i].view(n, n)
+                    m_rotated = p.U_L[i].T @ m @ p.U_R[i].T
+                    mb = self.hb_transform(m_rotated.view(1, -1)).view(n, n)
+                    scores = mb * c
+                    _, min_wi = torch.topk(scores.view(-1), min(group["num_flips"], n * n), largest=False)
+                    for min_i in min_wi:
+                        flipr = min_i // n
+                        flipc = min_i % n
+                        if scores[flipr, flipc] < 0:
+                            c[flipr, flipc] *= -1
+                    p.binary_coeffs[i] = c.view(-1)
+        return loss
+    
+    def hb_transform(self, x):
+        if len(x.shape) == 1:
+            x = x.view(1,-1)
+        (m,n) = x.shape
+        k = 1
+        while 4**k < n:
+            k += 1
+        assert(4**k == n)
+        x = x.reshape((m,) + (4,)*k)
+        for i in range(k):
+            x = x.sum(1+i,keepdim=True) - 2*x
+        x = x.reshape((m,) + (2,2)*k)
+        x = x.permute((0,) + tuple(2*i+1 for i in range(k)) + tuple(2*i+2 for i in range(k)))
+        return x.reshape(m, 2**k, 2**k) / (2**k)
